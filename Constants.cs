14844d76f72c633c63b1785412c64217f256808c
tomasmo-dev <68078006+tomasmo-dev@users.noreply.github.com>
2021-11-02 11:46:08 +0100

    added constants

---
+++ Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HtmlSocketServer
+{
+    class Constants
+    {
+        public static string SQLconString = "";
+        public static string SQLconDebugstr = "YOUR DEBUG SQL CONNECTION STRING"; //unrequired
+
+        public static void debuggerRes()
+        {
+            
+            // your debug variables
+        }
+
+    }
+}
